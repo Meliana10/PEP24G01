db1cc64be8cd575577cfa1eaef9239da77cfd480
Meliana10 <meliana_georgiana@yahoo.com>
2024-01-29 21:05:25 +0200

    modulul2

---
+++ modul2/part1.py
@@ -0,0 +1,21 @@
+str1 = 'hello world'
+print(type(str1))
+int1 = 100
+print(type(int1))
+none = None
+print(type(none))
+
+result = str1.capitalize()
+print('Returned type:', type(result))
+
+
+str1 = 'hello world {} {}'
+result = str1.format('test', 'test2')
+print('Centered String:', result)
+
+str1 = 'hello world'
+result = str1.center(21, '#')
+print('Centered String:', result)
+
+
+
