4867cde7bbc43dfac08658ca078223e824397658
Meliana10 <meliana_georgiana@yahoo.com>
2024-01-29 21:04:14 +0200

    modulul2

---
+++ modul2/app4.py
@@ -0,0 +1,14 @@
+# Se va cere input de la utilizator 2 numere. Se calculeaza:
+# a. media numerelor
+a = int(input('Introdu primul numar: '))
+b = int(input('Introdu al doilea numar: '))
+media = (a + b) / 2
+print(media)
+
+# b. impartirea primului nr la al doilea
+imp = a // b
+print("Integer division =", imp)
+
+# c. ridicarea la putere. al doilea nr la primul
+ridic = a ** b
+print("Ridicare la putere =", ridic)
